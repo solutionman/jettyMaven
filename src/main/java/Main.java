7e60219d97824afd1eda44ffe273a7286679fd06
lom <solutionman@inbox.ru>
2018-12-30 23:13:41 +0700

    request

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,20 +3,16 @@ import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
 public class Main {
-    public static void main(String[] args){
-        Frontend frontend = new Frontend();
+    public static void main(String[] args) throws Exception {
+        AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(allRequestsServlet), "/*");
 
         Server server = new Server(8080);
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         server.setHandler(context);
-        context.addServlet(new ServletHolder(frontend),"/*");
 
-        try {
-            server.start();
-            server.join();
-        } catch (Exception e){
-            System.out.println("Error: " + e);
-        }
-
+        server.start();
+        server.join();
     }
 }
